a8ca07fc63e9a13b67fd1e8387158125d4c1e2cd
Ilya Zelenin <wyster@make.im>
2021-01-14 03:59:48 -0800

    Read data from file and build bars

---
+++ data-handler.R
@@ -0,0 +1,30 @@
+library(dplyr)
+
+# read from file
+data <- read.table(
+  paste(getwd(), 'data.txt', sep = '/'),
+  header = TRUE,
+  sep = "\t",
+  quote=""
+)
+
+# columns list
+colnames(data)
+# rows length
+length(data)
+
+# min, max ID
+min(data$id)
+max(data$id)
+
+# handle gender
+gender <- data %>% count(data$gender)
+colnames(gender) <- c('name', 'count')
+gender
+
+# 1. Open jpeg file
+jpeg(paste(getwd(), 'genders.jpg', sep = '/'), width = 350, height = 350)
+# 2. Create the bar
+barplot(gender$count, names.arg = gender$name, horiz=TRUE)
+# 3. Close the file
+dev.off()
