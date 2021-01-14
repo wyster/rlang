a48fbc2eeb3479559fca4754f2f6ce189b025c63
Ilya Zelenin <wyster@make.im>
2021-01-14 04:56:07 -0800

    Fix & Cosmetics

--- data-handler.R
+++ data-handler.R
@@ -5,13 +5,13 @@ data <- read.table(
   paste(getwd(), 'data.txt', sep = '/'),
   header = TRUE,
   sep = "\t",
-  quote=""
+  quote = ""
 )
 
 # columns list
 colnames(data)
 # rows length
-length(data)
+nrow(data)
 
 # min, max ID
 min(data$id)
@@ -25,6 +25,6 @@ gender
 # 1. Open jpeg file
 jpeg(paste(getwd(), 'genders.jpg', sep = '/'), width = 350, height = 350)
 # 2. Create the bar
-barplot(gender$count, names.arg = gender$name, horiz=TRUE)
+barplot(gender$count, names.arg = gender$name, ylab = 'Rows')
 # 3. Close the file
 dev.off()
